20764140c176506f3f1fca78f638916aff7fdb1d
Peace Akhaze <peaceakhaze017@gmail.com>
2023-02-11 01:57:57 -0800

    project 2

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -13,4 +13,3 @@ elif ld == 0:
     print(f"Last digit of {number} is {ld} and is 0")
 elif ld < 6 and ld != 0:
     print(f"Last digit of {number} is {ld} and is less than 6 and not 0")
-
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,8 +2,8 @@
 for x in range(0, 10):
     for y in range(0, 10):
         if y == 9 and x == 8:
-            print(f"{x}{y}")
+            print(f"{0}{1}".format(x, y))
         elif y <= x:
             pass
         else:
-            print(f"{x}{y}, ",end="")
+            print("{0}{1}, ".format(x, y), end="")
