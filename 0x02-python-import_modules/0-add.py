f1777a827cd25b03924628c5d939370821adf92c
Peace Akhaze <peaceakhaze017@gmail.com>
2023-02-11 02:17:21 -0800

    check

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
-print("{0} + {1} = {2}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+
+    from add_0 import add
+
+    """imports add function from file and sums up two numbers"""
+
+    a = 1
+    b = 2
+    print("{0} + {1} = {2}".format(a, b, add(a, b)))
